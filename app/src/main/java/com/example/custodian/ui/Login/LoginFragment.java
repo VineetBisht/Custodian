b015f4a841d31d70bf9a5821272941ea6aa771ad
Vineet Singh <vineetbisht2013@gmail.com>
2020-04-04 05:13:03 -0400

    Initialized the basic layout of the application

---
+++ app/src/main/java/com/example/custodian/ui/Login/LoginFragment.java
@@ -0,0 +1,20 @@
+package com.example.custodian.ui.Login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.custodian.R;
+
+public class LoginFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
+        return root;
+    }
+}
